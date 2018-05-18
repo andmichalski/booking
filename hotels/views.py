95a06f533d9b05b7d07ef5ae0a6d8ad1a7ab5ea3
andmichalski <andrzmichal@gmail.com>
2018-05-18 16:40:05 +0200

    Add price functionality

--- hotels/views.py
+++ hotels/views.py
@@ -4,6 +4,7 @@ from django.views.generic import ListView
 from django.views.generic.edit import FormMixin
 from .models import Hotel
 from .forms import HotelForm
+from django.db.models import Min, Max
 from django.db.models import QuerySet
 from django.shortcuts import render
 
@@ -21,12 +22,22 @@ class MainView(ListView, FormMixin):
         if is_location:
             city = request.GET['location']
             objects = Hotel.objects.filter(location=city)
-        if is_standard:
+        elif is_standard:
             standard = request.GET['standard']
             if objects:
                 objects = objects.filter(standard=dict(Hotel.CLASSES_CHOICES)[standard])
             else:
                 objects = Hotel.objects.filter(standard=dict(Hotel.CLASSES_CHOICES)[standard])
+        else:
+            objects = Hotel.objects.all()
         self.object_list = objects
         context = self.get_context_data()
         return self.render_to_response(context)
+
+    def get_context_data(self, **kwargs):
+        context = super(MainView, self).get_context_data(**kwargs)
+        hotels = context['object_list']
+        hotels = hotels.annotate(min_prize = Min('room__prize'))
+        hotels = hotels.annotate(max_prize = Max('room__prize'))
+        context['object_list'] = hotels
+        return context
